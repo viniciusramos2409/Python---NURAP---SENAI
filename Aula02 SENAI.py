32e0d4991cc8ec2dfd59babd4938f24565654cda
Vini.r <vr2980380@gmail.com>
2024-12-03 11:30:38 -0300

    Add files via upload

---
+++ Aula02 SENAI.py
@@ -0,0 +1,11 @@
+print("Bom dia ao meu sever :)")
+
+nome_usuario = "ViniRamos" # O tipo -> str = string
+idade_usuario = 18 # O tipo -> int = integrer
+altura_usuario = 1.72 # O tipo -> float = ponto flutuante
+maior_idade = True # tipo -> bool = boolean
+
+print('seja bem-vindo' , nome_usuario)
+print('Sua idade é', idade_usuario, 'anos')
+print('Você tem', altura_usuario, "Metros")
+print("Você é maior de idade?", maior_idade)
